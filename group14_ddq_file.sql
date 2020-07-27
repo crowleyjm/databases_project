0a9f8417dc852cd17eddd6f27d09977952497226
Michael Kistler <michael@michaelkistler.net>
2020-07-27 13:32:05 -0400

    Complete data definition queries

--- group14_ddq_file.sql
+++ group14_ddq_file.sql
@@ -1,21 +1,57 @@
 CREATE TABLE `Museums` (
-    `museumID` int(11) NOT NULL AUTO_INCREMENT,
+    `museumID` INT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `city` VARCHAR(255) NOT NULL,
+    `country` VARCHAR(255) NOT NULL,
+    `museumType` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`museumID`)
 ) ENGINE = InnoDB;
 
 CREATE TABLE `Visitors` (
-    `visitorID` int(11) NOT NULL AUTO_INCREMENT,
+    `visitorID` INT(11) NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL,
+    `firstName` VARCHAR(255) NOT NULL,
+    `lastName` VARCHAR(255) NOT NULL,
+    `isMember` BOOLEAN,
+    PRIMARY KEY (`visitorID`)
 ) ENGINE = InnoDB;
 
 CREATE TABLE `Media` (
-    `mediaID` int(11) NOT NULL AUTO_INCREMENT,
+    `mediaID` INT(11) NOT NULL AUTO_INCREMENT,
+    `mediaType` VARCHAR(255) NOT NULL,
+    `name` VARCHAR(255)
+    `artist` VARCHAR(255)
+    `mediaDate` DATE,
+    `musuemID` INT(11) NOT NULL,
+    FOREIGN KEY (`museumID`) REFERENCES `Museums` (`museumID`),
+    PRIMARY KEY (`mediaID`)
 ) ENGINE = InnoDB;
 
 CREATE TABLE `Tours` (
-    `tourID` int(11) NOT NULL AUTO_INCREMENT,
+    `tourID` INT(11) NOT NULL AUTO_INCREMENT,
+    `startTime` DATETIME NOT NULL,
+    `endTime` DATETIME NOT NULL,
+    `price` DECIMAL(5, 2) NOT NULL,
+    `capacity` INT(11) NOT NULL,
+    `numberEnrolled` INT(11) NOT NULL,
+    `musuemID` INT(11) NOT NULL,
+    FOREIGN KEY (`museumID`) REFERENCES `Museums` (`museumID`),
+    PRIMARY KEY (`tourID`)
 ) ENGINE = InnoDB;
 
 CREATE TABLE `Museums_Visitors` (
+    `musuemID` INT(11) NOT NULL,
+    FOREIGN KEY (`museumID`) REFERENCES `Museums` (`museumID`),
+    `visitorID` INT(11) NOT NULL,
+    FOREIGN KEY (`visitorID`) REFERENCES `Visitors` (`visitorID`),
+    `visitDate` DATE,
+    PRIMARY KEY (`museumID`, `visitorID`, `visitDate`)
 ) ENGINE = InnoDB;
 
 CREATE TABLE `Tours_Visitors` (
+    `musuemID` INT(11) NOT NULL,
+    FOREIGN KEY (`museumID`) REFERENCES `Museums` (`museumID`),
+    `visitorID` INT(11) NOT NULL,
+    FOREIGN KEY (`visitorID`) REFERENCES `Visitors` (`visitorID`),
+    PRIMARY KEY (`museumID`, `visitorID`)
 ) ENGINE = InnoDB;
